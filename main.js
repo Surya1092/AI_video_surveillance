e89151459ac186519924e56b89f59037ebf47cb4
Surya1092 <96676153+Surya1092@users.noreply.github.com>
2022-03-15 14:00:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -24,3 +24,29 @@ function modelLoaded(){
     video.speed(1);
     video.volume(1);
 }
+
+function draw(){
+    image(video ,0 ,0, 480, 380);
+    if (status != "") {
+        object_detector.detect(video, gotResult);
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("ststus").innerHTML = "Status : Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number Of Objects Detected Are : " + objects.length;
+
+            fill("#ff0000");
+            percent = floor(objects[i].confidence*100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#0000ff");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height); 
+        }
+    }
+}
+
+function gotResult(error, results){
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
